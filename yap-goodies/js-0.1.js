9ed74abfc990e07e2e82e34c3d1e17a38c6c3a1d
codler <codler@gmail.com>
2010-08-18 20:09:28 +0200

    webbrowser icons and javascript functions (input_int)

---
+++ yap-goodies/js-0.1.js
@@ -0,0 +1,52 @@
+/*
+Name: yap-goodies
+Author: Han Lin Yap
+Description: Collection of useful functions
+Version: 0.1 (2010-03-29)
+
+Index:
+	input_int - (req jquery)
+	switch_display - (req jquery)
+ */
+ 
+//
+function input_int(event) {
+	var k = event.which;
+	var value = $(this).val();
+	
+	// Key arrow up
+	if (event.keyCode == 38) {
+		value++;
+		$(this).val(value);
+		return false;
+	// Key arrow down
+	} else if (event.keyCode == 40) {
+		value--;
+		$(this).val(value);
+		return false;
+	}
+	
+	// Key enter
+	if (k == 13) {
+		$(this).trigger('blur');
+	}
+	
+	// key 0-9
+	if ((k >= 32 && k <= 47) || (k >= 58 && k <= 126) || k > 127) {
+		// Key arrow left and right
+		if (event.keyCode != 37 && event.keyCode != 39) {
+		event.preventDefault();
+		}
+	}
+}
+
+// parameter - jquery object
+function switch_display(el1,el2) {
+	if (el1.is(":visible")) {
+		el1.hide();
+		el2.show();
+	} else {
+		el2.hide();
+		el1.show();
+	}
+}
