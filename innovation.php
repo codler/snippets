83e37d7ab489c201aee9f97c8be7223965aefb6b
codler <codler@gmail.com>
2010-12-03 11:02:22 +0100

    innovation

---
+++ innovation.php
@@ -0,0 +1,22 @@
+<?php 
+
+// if true do this
+true && echo "if true";
+
+// do if false
+false || echo "if true";
+
+// callback
+function call($callback) {
+	$callback();
+}
+
+// lambda function
+$lambda = function() {
+	echo "lambda function";
+}
+
+// learn to use
+// array_map(function, array);
+
+?>
